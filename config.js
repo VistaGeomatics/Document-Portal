d63ad7212e46b008a0f7b7329d8c03462a52debb
VIsta Geomatics <jasonm@vistageomatics.com>
2026-08-12 14:40:59 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.VISTA_PORTAL_CONFIG = {
-  supabaseUrl: 'PASTE_SUPABASE_PROJECT_URL_HERE',
-  supabaseAnonKey: 'PASTE_SUPABASE_ANON_KEY_HERE'
+  supabaseUrl: 'https://hicnkiuqghyxtnpxjkzn.supabase.co',
+  supabaseAnonKey: 'sb_publishable_BgMjiXg2DTcBk0JEEsYhlA_IHOPTC5L'
 };
